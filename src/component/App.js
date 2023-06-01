b548f8ea0fc4ff519db5bc4a6dd9ba07e1fc4b88
geh657 <2200515@ecc.ac.jp>
2023-06-01 11:59:15 +0900

    add:環境の構築

---
+++ src/component/App.js
@@ -0,0 +1,12 @@
+import Weather from ".Weather"
+import './App.css';
+
+function App() {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/weather.js
@@ -0,0 +1,10 @@
+function Weather() {
+    return (
+      <div className="p-4">
+        <div className="w-96 h-56 m-auto bg-blue-500">
+        </div>
+      </div>
+    )
+  }
+  
+  export default Weather
